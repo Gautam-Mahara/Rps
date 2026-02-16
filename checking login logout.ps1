3f037c29f977787720cf75fc789392a2302f9a3c
Gautam Singh Mahara <107118978+Gautam0612@users.noreply.github.com>
2026-02-16 12:09:15 +0530

    scripy

--- checking login logout.ps1
+++ checking login logout.ps1
@@ -1,5 +1,5 @@
 ﻿Get-WinEvent -FilterHashtable @{
-    LogName='Security';
-    ID=4624,4634;
-    StartTime=(Get-Date).AddHours(-2)
+ LogName='Security'
+ ID=4624,4634
+ StartTime=(Get-Date).AddMinutes(-120)
 }
